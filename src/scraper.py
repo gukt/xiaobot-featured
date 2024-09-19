ebf797e41ddbb82763fbc8bd4e66f8243d58855e
gukt <gukaitong@gmail.com>
2024-09-20 00:51:16 +0800

    feat: dev

--- src/scraper.py
+++ src/scraper.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from notion_client import Client
+from selenium.common.exceptions import WebDriverException 
 
 NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
 NOTION_DATABASE_ID = os.environ.get("NOTION_DATABASE_ID")
@@ -20,9 +21,20 @@ notion = Client(auth=NOTION_TOKEN)
 
 def setup_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # 运行在无头模式下
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
     service = Service(ChromeDriverManager().install())
-    return webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+        'source': '''
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+            })
+        '''
+    })
+    return driver
 
 def get_commission_rate(driver):
     # 获取佣金比例，要想获取到佣金比例数据，需要点击 div.share-paper 元素，点击后会在底部弹出一个模态对话框，里面有推广佣金比例信信息，
@@ -30,8 +42,16 @@ def get_commission_rate(driver):
         EC.presence_of_element_located((By.CSS_SELECTOR, ".share-paper"))
     )
 
+    # 移除页面中的 div.bottom_fixed 元素，这个元素会遮挡住 share-paper 元素
+    bottom_fixed_elem =  WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "bottom_fixed"))
+    )
+    driver.execute_script("arguments[0].remove();", bottom_fixed_elem)
+
     # span.text-theme 是推广佣金比例信息，如果找不到，就点击 share-paper 元素
     if not driver.find_elements(By.CSS_SELECTOR, "span.text-theme"):
+        print(f"点击 share-paper 元素")
+        share_paper_elem.location_once_scrolled_into_view  # 滚动到元素位置
         share_paper_elem.click()
 
     # 做一些保护措施：有时候元素不在当前视口中，导致被其他元素遮挡，所以需要滚动到元素
@@ -45,14 +65,34 @@ def get_commission_rate(driver):
     # 去掉百分号
     commission_rate = rate_elem.text.replace("%", "")
     # 如果为空，则设置为 0
-    return int(commission_rate) if commission_rate else 0
+    return int(commission_rate) / 100 if commission_rate else 0
 
 
-def get_tags_and_author(driver):
-    # 获取标签列表，如果找不到 tags, 就点击 category_elem 下的第二个元素
+def get_tags_and_author_and_last_update(driver):
+
+    # category_elem 是标签栏，第一个标签内容为专栏描述内容，它是默认显示的。
     category_elem = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "div.category"))
     )
+
+    # 获取专栏最后更新时间
+    # 查找所有 .post 元素，排除带有 .pin_label 的元素，取第一个元素的 .date 作为最后更新时间。如果没有剩余元素，则使用带有 .pin_label 的元素的 .date 作为最后更新时间。
+    posts_elem = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".posts"))
+    )
+    posts = posts_elem.find_elements(By.CSS_SELECTOR, ".post")
+    for post in posts:
+        if not post.find_elements(By.CSS_SELECTOR, ".pin_label"):
+            last_update_elem = post.find_element(By.CSS_SELECTOR, ".date")
+            break
+    else:
+        last_update_elem = posts[0].find_element(By.CSS_SELECTOR, ".date")
+    last_update = last_update_elem.text # '2024/01/02'
+    # # 转换为 date 类型
+    # NOTE：这里转换为 datetime 后，json 序列化时出错，所以保持字符串格式
+    # last_update = datetime.strptime(last_update, '%Y/%m/%d').date() if last_update else None
+
+    # 获取标签列表，如果找不到 tags, 就点击 category_elem 下的第二个元素
     if not driver.find_elements(By.CSS_SELECTOR, "span.tag"):
         category_elem.find_elements(By.CSS_SELECTOR, "div")[1].click()
 
@@ -72,30 +112,50 @@ def get_tags_and_author(driver):
     author_elem = driver.find_element(By.CSS_SELECTOR, ".post .name")
     author = author_elem.text
 
-    return tags, author
+    return tags, author, last_update
 
 
 def scrape_xiaobot(author_id):
     url = f"https://xiaobot.net/p/{author_id}"
     driver = setup_driver()
 
-    print(f'正在访问专栏页面：{url} ...')
-    driver.get(url)
+    print(f'正在获取专栏页面数据：{url} ...')
+    try:
+        driver.get(url)
+        time.sleep(5)  # 等待页面加载
+    except WebDriverException as e:
+        print(f"打开专栏页面时发生错误: {e}")
+        driver.quit()
+        return None
+    except Exception as e:
+        print(f"发生未知错误: {e}")
+        driver.quit()
+        return None
 
     try:
         print(f'正在解析页面数据...')
 
-        # 获取专栏名称
+        # 专栏名称
         title = driver.title
 
         # 根据 id=app 查找并等待元素加载成功
-        WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.ID, "app")) )
-
-        # 获取 Icon 链接地址
-        icon_elem = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "img.avatar"))
+        WebDriverWait(driver, 10).until( 
+            EC.presence_of_element_located((By.ID, "app")) 
         )
-        icon = icon_elem.get_attribute("src")
+
+        # 获得详细介绍
+        description_elem = WebDriverWait(driver, 10).until( 
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".description")) 
+        )
+        print('description: ', description_elem.get_attribute('innerHTML'))
+        description = description_elem.get_attribute('innerHTML')
+
+
+        # 头像地址
+        avatar_elem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".paper-info img.avatar"))
+        )
+        avatar = avatar_elem.get_attribute("src")
 
         # 获取阅读人数
         reader_count_elem = WebDriverWait(driver, 10).until(
@@ -116,21 +176,23 @@ def scrape_xiaobot(author_id):
         intro = intro_elem.text
 
         # 获取标签列表以及主理人信息
-        tags, author = get_tags_and_author(driver)
+        tags, author, last_update = get_tags_and_author_and_last_update(driver)
 
         # 获取佣金比例，要想获取到佣金比例数据，需要点击 div.share-paper 元素，点击后会在底部弹出一个模态对话框，里面有推广佣金比例信信息，
         commission_rate = get_commission_rate(driver)
 
         data = {
             "title": title,
-            "url": url,
+            "url": url + "?refer=f45527a5-d021-4bc8-958b-f28d2e90e7f7",
             "author_id": author_id,
             "author": author,
-            "icon": icon,
+            "avatar": avatar,
             "reader_count": int(reader_count),
             "article_count": int(article_count),
             "intro": intro,
+            "description": description,
             "commission_rate": commission_rate,
+            "last_update": last_update,
             "tags": tags,
         }
 
@@ -143,9 +205,21 @@ def scrape_xiaobot(author_id):
 
 def save_to_notion(data):
     author_id = data["author_id"]
+    cover = { "external": { "url": data["avatar"] } }
     properties = {
-        "名称": {"title": [{"text": {"content": data["title"]}}]},
-        "URL": {"url": data["url"]},
+        # 带链接地址的名称
+        "名称": {
+            "title": [
+                {
+                    "text": {
+                        "content": data["title"],
+                        "link": { "url": data["url"] }
+                    }
+                }
+            ]
+        },
+        "链接": {"url": data["url"]},
+        "头像": {"url": data["avatar"]},
         "专栏 ID": {"rich_text": [{"text": {"content": author_id}}]},
         "主理人": {"rich_text": [{"text": {"content": data["author"]}}]},
         "介绍": {"rich_text": [{"text": {"content": data["intro"]}}]},
@@ -153,9 +227,18 @@ def save_to_notion(data):
         "文章数": {"number": data["article_count"]},
         "佣金比例": {"number": data["commission_rate"]},
         "内容标签": {"multi_select": [{"name": tag} for tag in data["tags"]]},
-        "同步时间": {"date": {"start": datetime.now(timezone.utc).isoformat()}}, # 注意这里要使用 UTC 时间
-        "状态": {"status": {"name": "Done"}},
-        }
+        "同步时间": {
+            "date": {
+                "start": datetime.now(timezone.utc).isoformat() # 使用 UTC 时间
+            }
+        },
+        "专栏最近更新时间": {
+            "date": {
+                "start": data["last_update"].replace("/", "-") # ISO 8601
+            }
+        },
+        "同步状态": {"status": {"name": "Done"}},
+    }
 
     print(f"[Notion] 正在检查专栏 {author_id} 页面是否存在...")
 
@@ -175,6 +258,8 @@ def save_to_notion(data):
         print(f"[Notion] 页面已存在, 执行更新操作")
         notion.pages.update(
             page_id=page_id,
+            cover=cover,
+            icon=cover,
             properties=properties
         )
     # 如果未找到该专栏 ID 的页面，则创建新页面
@@ -182,15 +267,20 @@ def save_to_notion(data):
         print(f"[Notion] 页面不存在, 创建新页面.")
         notion.pages.create(
             parent={"database_id": NOTION_DATABASE_ID},
+            cover=cover,
+            icon=cover,
             properties=properties,
             children=[
                 {
                     "object": "block",
                     "type": "image",
-                    "image": {
-                        "external": {
-                            "url": data["icon"]
-                        }
+                    "image": { "external": { "url": data["avatar"] } }
+                },
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "text": [{"text": {"content": data["description"]}}]
                     }
                 }
             ]
@@ -216,14 +306,20 @@ def save_to_file(data):
 
 def main():
     with open('authors.txt', 'r') as f:
-        authors = f.read().splitlines()
+        # 忽略空行
+        authors = [author for author in f.read().splitlines() if author.strip()]
 
     for author in authors:
         data = scrape_xiaobot(author)
-        # 保存到本地文件
-        save_to_file(data)
-        # 保存到 Notion
-        save_to_notion(data)
+
+        if data:
+            # 保存到本地文件
+            save_to_file(data)
+            # 保存到 Notion
+            save_to_notion(data)
+
+        print(f"等待 10 秒...")
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
